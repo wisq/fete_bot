6b7fa720ebb742a8cb084edb3dee87c7f9673111
Adrian Irving-Beer <wisq@wisq.net>
2022-11-12 18:46:44 -0500

    Add date to logger.

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,8 @@ config :fete_bot,
 
 config :fete_bot, FeteBot.Application, start_test: false
 
+config :logger, :console, format: "$date $time [$level] $message\n"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
