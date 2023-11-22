22088fc58809379e971c743ae872775e4ad2cda7
Adrian Irving-Beer <wisq@wisq.net>
2023-11-22 18:50:44 -0500

    Reduce number of Bandit acceptors.

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,10 @@ config :fete_bot,
   ecto_repos: [FeteBot.Repo]
 
 config :fete_bot, FeteBot.Application, start_test: false
-config :fete_bot, FeteBot.HealthCheck, enabled: false
+
+config :fete_bot, FeteBot.HealthCheck,
+  enabled: false,
+  thousand_island_options: [num_acceptors: 3]
 
 config :logger, :console, format: "$date $time [$level] $message\n"
 
